06789379a752c55e7bfd505da5dc70bea3f64e61
Supermoox <mooxmk@yahoo.co.uk>
2020-09-13 17:56:25 +0200

    added publish to active admin

--- app/admin/tournament.rb
+++ app/admin/tournament.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Tournament do
-  permit_params :name, :mode, :completed, teams_attributes: [:id, :_destroy, :name, :points, :host, :seeded,  :tournament_id]
+  permit_params :name, :mode, :publish, :completed, teams_attributes: [:id, :_destroy, :name, :points, :host, :seeded,  :tournament_id]
 end
